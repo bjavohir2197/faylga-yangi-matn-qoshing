e0e25247c008a89482cbb4796f33f445e8c941b6
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 12:28:46 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Faylga yangi matn qoshing (append rejimida) va keyin fayl mazmunini ekranga chiqaring.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
